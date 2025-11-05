20d686e01187572a225b1864cd428fcc699e6146
Luciana Garay <sasluciana123@gmail.com>
2025-11-05 16:25:30 -0300

    Descripción del cambiogit commit -m

--- app/src/main/java/com/example/gestorg3/UsuarioAdapter.java
+++ app/src/main/java/com/example/gestorg3/UsuarioAdapter.java
@@ -28,7 +28,7 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.ViewHold
 
     @Override
     public UsuarioAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_usuario, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_registro, parent, false);
         return new ViewHolder(v);
     }
 
